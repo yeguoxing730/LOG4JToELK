6a9dbcf7591618848512b210523ef9601148efdd
yeguoxing730 <yeguoxing730@163.comm>
2018-02-18 23:12:39 +0800

    update code

--- src/main/java/Log4JToELK.java
+++ src/main/java/Log4JToELK.java
@@ -1,15 +1,18 @@
 import org.apache.log4j.Logger;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by yeguo on 2018/2/16.
  */
 public class Log4JToELK {
     private static final Logger logger = Logger.getLogger(Log4JToELK.class);
     public static void main(String[] args) throws Exception {
-        logger.debug("This is a debug message!");
-        logger.info("This is info message!");
-        logger.warn("This is a warn message!");
-        logger.error("This is error message!");
+        LocalDateTime now = LocalDateTime.now();
+        logger.debug("This is a debug message!"+now);
+        logger.info("This is info message!"+now);
+        logger.warn("This is a warn message!"+now);
+        logger.error("This is error message!"+now);
         try{
             System.out.println(5/0);
         }catch(Exception e){
